9b7ffa94249561963b15a209bedbbbdab7391aeb
Swrangsar Basumatary <swrangsar@aol.com>
2012-09-14 04:48:37 +0530

    deletes

--- printTree23.h
+++
@@ -1,8 +0,0 @@
-#ifndef _printTree23_h
-#define _printTree23_h
-
-void printTree23LevelOrder(tree23* tree);
-void printNode23LevelOrder(tree23Node* node);
-void printNode23(tree23Node* node);
-
-#endif
